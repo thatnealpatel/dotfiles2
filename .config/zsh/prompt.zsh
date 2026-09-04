93fc620baa990fed70289b6ad7fb8c2de6947747
Neal Patel <neal@patel.codes>
2026-09-04 13:01:24 -0400

    zsh: blank line before each prompt

--- .config/zsh/prompt.zsh
+++ .config/zsh/prompt.zsh
@@ -1,8 +1,10 @@
-# prompt.zsh: two lines, no VCS state (jj has its own).
+# prompt.zsh: a blank line, then two lines, no VCS state (jj has its own).
+#
 #
 #   <host>:<cwd>
 #   %
 
-PS1='%F{red}%M%f:%F{blue}%~%f
+PS1='
+%F{red}%M%f:%F{blue}%~%f
 %F{244}%# %f'
 RPROMPT=''
